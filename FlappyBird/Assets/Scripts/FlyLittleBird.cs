036d79c126e91044f97cd4a5e1df538a0bb1c49d
Long Thai <n10006257@qut.edu.au>
2019-05-06 00:06:58 +1000

    Added bird movement, collisions, added pipes

---
+++ FlappyBird/Assets/Scripts/FlyLittleBird.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FlyLittleBird : MonoBehaviour {
+
+	public float velocity = 1;
+	public Rigidbody2D rb;
+	private bool tapped = false;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			tapped = true;
+		} else { tapped = false; }
+	}
+
+	void FixedUpdate () {
+		if (tapped == true) {
+			rb.velocity = Vector2.up * velocity;
+		}
+	}
+}
