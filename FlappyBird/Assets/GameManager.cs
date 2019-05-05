6f3c6633b1ed9d7a8bfc3023b0ca4c9926d28601
Long Thai <n10006257@qut.edu.au>
2019-05-06 01:28:16 +1000

    Added scoring

--- FlappyBird/Assets/GameManager.cs
+++ FlappyBird/Assets/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
@@ -13,6 +14,10 @@ public class GameManager : MonoBehaviour {
 	public void GameOver() {
 		gameOverCanvas.SetActive(true);
 		Time.timeScale = 0;
+	}
+
+	public void Replay() {
+		SceneManager.LoadScene(0);
 	}
 
 }
